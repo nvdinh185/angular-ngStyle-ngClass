1d01b17a358e688bbb72126cdbc4da4ca3b22c67
nvdinh185 <nvdinh0905671805@gmail.com>
2019-08-30 22:30:03 +0700

    first commit

---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+  name = '';
+  isHighLight = false;
+  b = { circle: true };
+  evenStyle = { color: 'red', fontSize: '20px', backgroundColor: 'gray' };
+  oddStyle = { color: 'black', fontSize: '40px', backgroundColor: 'green' };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
